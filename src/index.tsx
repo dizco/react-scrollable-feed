d2f89d8dad732b0babe93252f806429e61b0a524
Gabriel <dizco@users.noreply.github.com>
2018-10-03 18:19:06 -0400

    Change visible detection strategy to take parent into account

--- src/index.tsx
+++ src/index.tsx
@@ -8,48 +8,74 @@ export interface ScrollableFeedProps {
 }
 
 class ScrollableFeed extends React.Component<ScrollableFeedProps> {
+  private wrapperRef: React.RefObject<HTMLDivElement>;
   private bottomRef: React.RefObject<HTMLDivElement>;
 
   constructor(props: ScrollableFeedProps) {
     super(props);
     this.bottomRef = React.createRef();
+    this.wrapperRef = React.createRef();
   }
 
   getSnapshotBeforeUpdate() {
-    return this.visibleY(this.bottomRef.current); //This argument is passed down to componentDidUpdate as 3rd parameter
+    return this.isViewable(this.wrapperRef.current, this.bottomRef.current); //This argument is passed down to componentDidUpdate as 3rd parameter
   }
 
   componentDidUpdate({}: any, {}: any, snapshot: any) {
     console.log('Comonent did update', this.state, this.props, snapshot);
-    if (snapshot && this.bottomRef.current) {
-      this.bottomRef.current.scrollIntoView({behavior: 'smooth'});
+    if (snapshot && !this.isViewable(this.wrapperRef.current, this.bottomRef.current)
+      && this.bottomRef && this.bottomRef.current
+      && this.wrapperRef && this.wrapperRef.current) {
+      this.scrollParentToChild(this.wrapperRef.current, this.bottomRef.current);
     }
+  }
+
+  private scrollParentToChild(parent: any, child: any) {
+    //Source: https://stackoverflow.com/a/45411081/6316091
+    // Where is the parent on page
+    var parentRect = parent.getBoundingClientRect();
+    // What can you see?
+    var parentViewableArea = {
+      height: parent.clientHeight,
+      width: parent.clientWidth
+    };
+
+    // Where is the child
+    var childRect = child.getBoundingClientRect();
+    // Is the child viewable?
+    var isViewable = (childRect.top >= parentRect.top) && (childRect.top <= parentRect.top + parentViewableArea.height);
+
+    // if you can't see the child try to scroll parent
+    if (!isViewable) {
+      // scroll by offset relative to parent
+      parent.scrollTop = (childRect.top + parent.scrollTop) - parentRect.top
+    }
+  }
+
+  private isViewable(parent: any, child: any): boolean {
+    // Where is the parent on page
+    var parentRect = parent.getBoundingClientRect();
+    // What can you see?
+    var parentViewableArea = {
+      height: parent.clientHeight,
+      width: parent.clientWidth
+    };
+
+    // Where is the child
+    var childRect = child.getBoundingClientRect();
+    // Is the child viewable?
+    return (childRect.top >= parentRect.top) && (childRect.top <= parentRect.top + parentViewableArea.height);
   }
 
   render() {
     const children = this.props.children;
     return (
-      <div className={styles.scrollableDiv} style={{maxHeight: this.props.maxHeight}}>
+      <div className={styles.scrollableDiv} style={{maxHeight: this.props.maxHeight}} ref={this.wrapperRef}>
         {children}
         <div ref={this.bottomRef}></div>
       </div>
     );
   }
-
-  private visibleY(el: any): boolean{
-    //Source: https://stackoverflow.com/a/21627295/6316091
-    var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height,
-      el = el.parentNode;
-    do {
-      rect = el.getBoundingClientRect();
-      if (top > rect.bottom) return false;
-      // Check if the element is out of view due to a container scrolling
-      if ((top + height) <= rect.top) return false
-      el = el.parentNode;
-    } while (el != document.body);
-    // Check its within the document viewport
-    return top <= document.documentElement.clientHeight;
-  };
 }
 
 export default ScrollableFeed;
