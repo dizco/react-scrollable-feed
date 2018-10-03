e9d81f4b815998e6a5d07717868baf74aead3b28
Gabriel <dizco@users.noreply.github.com>
2018-10-02 22:23:20 -0400

    Initial working state

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,35 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
-import ExampleComponent from 'react-scrollable-feed'
+import ScrollableFeed from 'react-scrollable-feed';
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      console.log('oh come on', this.state.items);
+      this.setState(prevState => ({
+        items: [...prevState.items, new Date().toISOString()]
+      }))
+    }, 3000);
+  }
+
+  state = {
+    items: ['TARTE', 'TARTE2', 'Je suis pas une tarte ok?'],
+  };
+
   render () {
+    const { items } = this.state;
     return (
-      <div>
-        <ExampleComponent text='Modern React component module' />
+      <div className={'scrollable-wrapper'}>
+        <ScrollableFeed maxHeight={300}>
+          <p>Tu me troll</p>
+          <p>Je te troll</p>
+          <div>Salut</div>
+          <p>Bof</p>
+          {items.map((item, i) => <div key={i}>{item}</div>)}
+        </ScrollableFeed>
       </div>
-    )
+    );
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,55 @@
-/**
- * @class ExampleComponent
- */
-
 import * as React from 'react'
 
 import styles from './styles.css'
 
-export type Props = { text: string }
-
-export default class ExampleComponent extends React.Component<Props> {
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+export interface ScrollableFeedProps {
+  maxHeight: number;
+  scrollBehavior?: string; //TODO: Default to auto or smooth?
 }
+
+class ScrollableFeed extends React.Component<ScrollableFeedProps> {
+  private bottomRef: React.RefObject<HTMLDivElement>;
+
+  constructor(props: ScrollableFeedProps) {
+    super(props);
+    this.bottomRef = React.createRef();
+  }
+
+  getSnapshotBeforeUpdate() {
+    return this.visibleY(this.bottomRef.current); //This argument is passed down to componentDidUpdate as 3rd parameter
+  }
+
+  componentDidUpdate({}: any, {}: any, snapshot: any) {
+    console.log('Comonent did update', this.state, this.props, snapshot);
+    if (snapshot && this.bottomRef.current) {
+      this.bottomRef.current.scrollIntoView({behavior: 'smooth'});
+    }
+  }
+
+  render() {
+    const children = this.props.children;
+    return (
+      <div className={styles.scrollableDiv} style={{maxHeight: this.props.maxHeight}}>
+        {children}
+        <div ref={this.bottomRef}></div>
+      </div>
+    );
+  }
+
+  private visibleY(el: any): boolean{
+    //Source: https://stackoverflow.com/a/21627295/6316091
+    var rect = el.getBoundingClientRect(), top = rect.top, height = rect.height,
+      el = el.parentNode;
+    do {
+      rect = el.getBoundingClientRect();
+      if (top > rect.bottom) return false;
+      // Check if the element is out of view due to a container scrolling
+      if ((top + height) <= rect.top) return false
+      el = el.parentNode;
+    } while (el != document.body);
+    // Check its within the document viewport
+    return top <= document.documentElement.clientHeight;
+  };
+}
+
+export default ScrollableFeed;
