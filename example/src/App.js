7daefdbd65caca9862fcd0ab216e3676905220b3
Gabriel <dizco@users.noreply.github.com>
2018-10-04 11:57:15 -0400

    Update demo

--- example/src/App.js
+++ example/src/App.js
@@ -7,29 +7,57 @@ export default class App extends Component {
   constructor(props) {
     super(props);
     setInterval(() => {
-      console.log('oh come on', this.state.items);
-      this.setState(prevState => ({
-        items: [...prevState.items, new Date().toISOString()]
-      }))
-    }, 3000);
+      this.addItem();
+    }, 800);
   }
 
   state = {
-    items: ['TARTE', 'TARTE2', 'Je suis pas une tarte ok?'],
+    items: [
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      'Aliquam semper purus vitae commodo elementum.',
+      'Maecenas condimentum lacus eget varius accumsan.',
+      'Mauris mattis orci sit amet mi consequat tristique.',
+      'Aliquam lobortis pulvinar fermentum.',
+      'Donec malesuada, sem non scelerisque imperdiet.',
+      'Quisque egestas.',
+      'Etiam scelerisque tincidunt enim, in dictum metus dapibus.',
+      'Quisque eu eros mattis, finibus lorem ut, commodo ipsum.',
+      'Vivamus cursus tempor nibh ac aliquet.',
+      'Nulla posuere gravida ligula tincidunt efficitur.',
+      'Phasellus vel convallis neque, id placerat elit.',
+      'Aenean nibh purus, consectetur hendrerit egestas in.',
+      'Aliquam pretium, sapien tempus posuere eleifend.',
+      'Sed eget viverra.',
+    ],
   };
 
-  render () {
+  addItem() {
+    this.setState(prevState => ({
+      items: [...prevState.items, new Date().toISOString()]
+    }));
+  }
+
+  render() {
     const { items } = this.state;
     return (
-      <div className={'scrollable-wrapper'}>
-        <ScrollableFeed maxHeight={300}>
-          <p>Tu me troll</p>
-          <p>Je te troll</p>
-          <div>Salut</div>
-          <p>Bof</p>
-          {items.map((item, i) => <div key={i}>{item}</div>)}
-        </ScrollableFeed>
-      </div>
+      <section>
+        <div className={'scrollable-wrapper'} style={{maxHeight: 300}}>
+          <ScrollableFeed
+            changeDetectionFilter={(previousProps, newProps) => {
+              const prevChildren = previousProps.children;
+              const newChildren = newProps.children;
+
+              return prevChildren !== newChildren
+                && prevChildren[prevChildren.length - 1] !== newChildren[newChildren.length - 1];
+            }}
+          >
+            {items.map((item, i) => <div key={i}>{item}</div>)}
+          </ScrollableFeed>
+        </div>
+        <div style={{textAlign: 'center'}}>
+          <button onClick={() => this.addItem()}>Add Item</button>
+        </div>
+      </section>
     );
   }
 }
