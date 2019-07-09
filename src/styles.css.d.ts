45ecd32cc2cc42501a951381af8bb37f8e8891ee
Gabriel <dizco@users.noreply.github.com>
2019-07-09 08:49:41 -0400

    Update dependencies (#12)

--- src/styles.css.d.ts
+++ src/styles.css.d.ts
@@ -1,4 +1,7 @@
-declare namespace styles {
-	const scrollableDiv: string
+export const scrollableDiv: string
+interface Namespace {
+	"scrollableDiv": string,
+	"scrollable-div": string,
 }
+declare const styles: Namespace
 export default styles
