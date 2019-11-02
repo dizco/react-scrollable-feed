549ee21343daa7c60a1a45913054cc20b1d77783
Gabriel <dizco@users.noreply.github.com>
2019-11-02 18:01:21 -0400

    bootstrap min

--- example/src/index.js
+++ example/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './index.css'
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App'
 
 ReactDOM.render(<App />, document.getElementById('root'))
